66fcadfc839b894fca087bc76e5adc3b524aad08
Nazarii <Kyzenko.nr@gmail.com>
2024-06-22 11:14:29 +0300

    make build

---
+++ index.b9dc27c2.js
@@ -0,0 +1,2 @@
+document.querySelector(".gallery").addEventListener("click",function(e){var t=e.target.closest(".list-item__link");t&&(e.preventDefault(),document.getElementById("largeImg").src=t.href)});
+//# sourceMappingURL=index.b9dc27c2.js.map
